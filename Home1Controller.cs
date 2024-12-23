fd0dc9c4b856ec058f45befa0e7a425411a64ab4
tasnimalmobid <134017604+tasnimalmobid@users.noreply.github.com>
2024-12-23 20:11:58 +0300

    Add files via upload

---
+++ Home1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
